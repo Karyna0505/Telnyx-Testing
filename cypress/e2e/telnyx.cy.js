4c99ec8a25a62fb00e687c17d3ae9e5bda7d285f
Karyna0505 <karina.ivannikova96@gmail.com>
2022-12-19 16:10:58 +0100

    new

--- cypress/e2e/telnyx.cy.js
+++ cypress/e2e/telnyx.cy.js
@@ -28,7 +28,7 @@ describe('Testing Telnyx site', function () {
           });
     });
     it('TS_0001_2', function (){  
-        cy.get('body > div > div > footer').scrollIntoView();       
+        cy.get('#__next > div > footer').scrollIntoView();       
         cy.contains('Follow on Twitter').invoke('removeAttr', 'target').click().wait(6000);
         cy.url().should('include','telnyx');
            
@@ -36,7 +36,7 @@ describe('Testing Telnyx site', function () {
      
     it('TS_0001_3', function (){
 
-          cy.get('body > div > div > footer').scrollIntoView();
+          cy.get('#__next > div > footer').scrollIntoView();
           cy.get('footer > div.sc-7b6c9f9b-3.iznSjj > div > div:nth-child(6) > div > ul > li:nth-child(3) > a')
           .then($elem => {
             $elem[0].removeAttribute('target');
